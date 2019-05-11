5fb465bf799789ea2eaa5516c17ef29b877aad5e
root <root@PC-YFB-HXB.localdomain>
2019-05-11 17:01:49 +0800

    add build_querystring log

--- contrib/dlz/modules/common/dlz_dbi.c
+++ contrib/dlz/modules/common/dlz_dbi.c
@@ -286,11 +286,8 @@ build_querystring(query_list_t *querylist) {
 		if (tseg->direct == true)
 			length += tseg->strlen;
 		else	/* calculate string length for dynamic segments. */
-			log(ISC_LOG_DEBUG(1),
-				"build_querystring tseg->direct=");
-			log(ISC_LOG_DEBUG(1), tseg->direct);
-			log(ISC_LOG_DEBUG(1), "tseg->cmd=%s");
-			log(ISC_LOG_DEBUG(1), *(char **)tseg->cmd);
+			printf("build_querystring tseg->direct=%d tseg->cmd=%s", 
+				tseg->direct, *(char **)tseg->cmd);
 			length += strlen(* (char**) tseg->cmd);
 		/* get the next segment */
 		tseg = DLZ_LIST_NEXT(tseg, link);
